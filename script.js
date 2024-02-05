db654ae039c9686983ec6ba6afd9e70ea9923941
JohnnyWilkerson <149409298+JohnnyWilkerson@users.noreply.github.com>
2024-02-05 16:54:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFAWqxQMkTGUN7LwAMk-cnWjaiUBLrmTY",
+    authDomain: "checklist-4824c.firebaseapp.com",
+    databaseURL: "https://checklist-4824c-default-rtdb.firebaseio.com",
+    projectId: "checklist-4824c",
+    storageBucket: "checklist-4824c.appspot.com",
+    messagingSenderId: "493740534465",
+    appId: "1:493740534465:web:75216e6bed62d0c3934240"
+  };

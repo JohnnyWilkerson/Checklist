9e538a3cdfa8db1e38c57b7eb70596840fedead4
JohnnyWilkerson <149409298+JohnnyWilkerson@users.noreply.github.com>
2024-02-11 19:37:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const firebaseConfig = {
     messagingSenderId: "493740534465",
     appId: "1:493740534465:web:75216e6bed62d0c3934240"
   };
+
+  firebase.initializeApp(firebaseConfig);
+getData();
+
+function add(){
+  item = document.getElementById("inpt").value;
+  firebase.database().ref("/").child(item).update({
+    purpose: "adding message"
+  });
+  getData();
+}
+
+function getData(){
+  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+    item = childKey;
+  row = "<div>" + item + "</div>";
+  document.getElementById("output").innerHTML += row;
+  });});}
+
+function del(){
+  tbd = document.getElementById("inpt").value;
+  firebase.database().ref("/").child(tbd).remove();
+}
